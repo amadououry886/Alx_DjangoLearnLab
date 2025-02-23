438d4970fc77dcd70ec2aecc7e734c7c9f14858d
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-02-24 04:24:24 +0800

    templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Book  # Import the Book model
 from django.views.generic.detail import DetailView
 from .models import Library  # Import the Library model
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 def list_books(request):
     books = Book.objects.all()  # Fetch all books from the database
@@ -11,3 +14,20 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  # Template to render
     context_object_name = 'library'  # The name to use in the template
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log the user in
+            return redirect('list_books')  # Redirect to books page
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+def custom_logout(request):
+    logout(request)
+    return redirect('login')  # Redirect to login page
+
