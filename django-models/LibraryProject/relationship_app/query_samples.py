fd4367f3e0d72ad479b8a2b53ee522d52d715d9d
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-02-24 02:24:35 +0800

    updated

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,7 +9,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 def query_books_by_author(author_name):
     """Retrieve all books by a specific author."""
-    author = Author.objects.filter(name=author_name).first()
+    author = Author.objects.get(name=author_name).first()
     if author:
         books = Book.objects.filter(author=author)  # Corrected: using `author` object, not a string
         return [book.title for book in books]
