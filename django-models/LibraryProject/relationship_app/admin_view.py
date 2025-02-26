46ae4bd35db16b96f6fed238f4abf543fe2c827d
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-02-26 19:50:24 +0800

    admin_viewj

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    user.userprofile.role == 'Admin'
 
-@user_passes_test(is_admin)
-def admin_dashboard(request):
-    return render(request, 'admin_dashboard.html', {"message": "Welcome, Admin!"})
+@user_passes_test(is_admin, login_url="login")
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
