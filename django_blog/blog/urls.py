20becfb2b7a8a4ff927abfdf9337b7a83a99fc93
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-03-18 01:12:48 +0800

    I have add some comment and delete or udpdate files for the projecf

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     register, user_login, user_logout, profile, 
-    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, add_comment, CommentUpdateView, CommentDeleteView
 )
 
 urlpatterns = [
@@ -17,4 +17,8 @@ urlpatterns = [
     path("post/new/", PostCreateView.as_view(), name="post-create"),  # Create new post
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),  # Edit post
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),  # Delete post
+
+    path("posts/<int:post_id>/comments/new/", add_comment, name="comment-add"),
+    path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-edit"),
+    path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
