7b7a44ccd84b7521a8cf95cc2b43c2258da5a0ef
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-03-27 13:51:16 +0800

    add some fonctionalities for the post and comment section

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,14 @@
 
 from django.urls import path
 from accounts.views import RegisterView, LoginView, ProfileView  # Explicit relative import
+from .views import follow_user, unfollow_user
+
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),  # Explicit path match
     path("login/", LoginView.as_view(), name="login"),  # Explicit path match
     path("profile/", ProfileView.as_view(), name="profile"),  # Explicit path match
+    path('follow/<int:user_id>/', follow_user, name='follow-user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
+
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from accounts.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 User = get_user_model()
 
@@ -27,3 +31,17 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserProfileSerializer(request.user)  # ✅ Use UserProfileSerializer
         return Response(serializer.data)
+    
+api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    request.user.follow(user_to_follow)
+    return Response({"message": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    request.user.unfollow(user_to_unfollow)
+    return Response({"message": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
