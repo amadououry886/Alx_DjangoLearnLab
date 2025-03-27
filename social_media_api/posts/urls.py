22d5ef0bd6a9aaf02475cf52164e7dff3d82c3d0
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-03-27 14:41:53 +0800

    some changes

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from posts.views import PostViewSet, CommentViewSet
-from .views import user_feed
+from .views import user_feed, FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -10,4 +10,5 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', user_feed, name='user-feed'),
+    path('feed/', FeedView.as_view(), name='feed'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics, permissions
 from posts.models import Post, Comment
 from posts.serializers import PostSerializer, CommentSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Post
+from .serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -34,4 +35,18 @@ def user_feed(request):
     posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
+
+
+
+
+class FeedView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        # Get the list of users the current user follows
+        following_users = self.request.user.following.all()
+        # Fetch posts from followed users, ordered by creation date (most recent first)
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+
 
